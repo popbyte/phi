1fcb7bb962d8a5d934a7a95582a150ad380c7065
popbyte <iidvar+github@proton.me>
2025-04-09 17:55:11 +0700

    fix: use Gemini GenAI SDK AsyncClient and handle overflowing correctly

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ intents.message_content = True
 bot = commands.Bot(command_prefix="ph!", intents=intents)
 settings = json.load(open("settings.json", "r"))
 loading_emoji = settings["loading_emoji"]
-gemini = genai.Client(api_key=settings["gemini_ai_token"])
+gemini = genai.Client(api_key=settings["gemini_ai_token"]).aio
 httpx = Client()
 
 @bot.event
@@ -20,26 +20,37 @@ async def on_ready():
 async def chat(ctx: commands.Context):
     message = await ctx.send(loading_emoji)
     
-    response = gemini.models.generate_content_stream(
+    response = await gemini.models.generate_content_stream(
         model="gemini-2.0-flash",
         contents=[ctx.message.content[8:]]
     )
+
     response_chunk = ""
-    for chunk in response:
+    async for chunk in response:
         response_chunk += chunk.text
+        response_len = len(response_chunk)
+        if response_len > 2000:
+            response_chunk = response_chunk[response_len - 2000:]
+            message = await ctx.send(response_chunk)
+            continue
         await message.edit(response_chunk)
 
 @bot.slash_command(description="Chat with Gemini 2.0 Flash")
 async def chat(interaction: nextcord.Interaction, message: str):
-    message = await interaction.send(loading_emoji)
+    bot_message = await interaction.send(loading_emoji)
     
-    response = gemini.models.generate_content_stream(
+    response = await gemini.models.generate_content_stream(
         model="gemini-2.0-flash",
         contents=[message]
     )
     response_chunk = ""
-    for chunk in response:
+    async for chunk in response:
         response_chunk += chunk.text
-        await message.edit(response_chunk)
+        response_len = len(response_chunk)
+        if response_len > 2000:
+            response_chunk = response_chunk[response_len - 2000:]
+            bot_message = await interaction.send(response_chunk)
+            continue
+        await bot_message.edit(response_chunk)
  
 bot.run(settings["bot_token"])
