800c48e33ba08c64c7ab9a471249d6ef276f6267
popbyte <iidvar+github@proton.me>
2025-04-10 20:58:07 +0700

    improvements

--- main.py
+++ main.py
@@ -1,16 +1,26 @@
 import nextcord
 from nextcord.ext import commands
 from google import genai
-from httpx import Client
+from google.genai import types
 import json
 
 intents = nextcord.Intents.default()
 intents.message_content = True
 bot = commands.Bot(command_prefix="ph!", intents=intents)
 settings = json.load(open("settings.json", "r"))
-loading_emoji = settings["loading_emoji"]
+loading_emoji = settings["loading_emoji"] or "\N{ZERO WIDTH NON-JOINER}"
 gemini = genai.Client(api_key=settings["gemini_ai_token"]).aio
-httpx = Client()
+gemini_config = types.GenerateContentConfig(
+    tools=[
+        types.Tool(
+        google_search = types.GoogleSearch()
+        )
+    ],
+    response_modalities=["TEXT"],
+    temperature = settings.get("temperature", 0.5),
+    topP = settings.get("topP", 1),
+    topK = settings.get("topK", 0),
+)
 
 @bot.event
 async def on_ready():
@@ -22,7 +32,8 @@ async def chat(ctx: commands.Context):
     
     response = await gemini.models.generate_content_stream(
         model="gemini-2.0-flash",
-        contents=[ctx.message.content[8:]]
+        contents=[ctx.message.content[8:]],
+        config=gemini_config
     )
 
     response_chunk = ""
@@ -41,7 +52,8 @@ async def chat(interaction: nextcord.Interaction, message: str):
     
     response = await gemini.models.generate_content_stream(
         model="gemini-2.0-flash",
-        contents=[message]
+        contents=[message],
+        config=gemini_config
     )
     response_chunk = ""
     async for chunk in response:
